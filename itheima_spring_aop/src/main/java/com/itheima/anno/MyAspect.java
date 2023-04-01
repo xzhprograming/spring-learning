1e859ba24fee951bc94438f6ece9fe38a380efee
xing <1244838363@qq.com>
2023-04-01 19:37:55 +0800

    AOP执行顺序

--- itheima_spring_aop/src/main/java/com/itheima/anno/MyAspect.java
+++ itheima_spring_aop/src/main/java/com/itheima/anno/MyAspect.java
@@ -4,17 +4,21 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
+
+/**
+ * AOP执行顺序
+ * 正常情况：Around -> Before -> method invoke -> Around -> After -> AfterReturning
+ * 异常情况：Around -> Before -> method invoke -> Around -> After -> AfterThrowing
+ */
+
 @Component("myAspect")
 @Aspect //标注当前MyAspect是一个切面类
 public class MyAspect {
     //配置前置通知
     //@Before("execution(* com.itheima.anno.*.*(..))")
+    @Before("pointcut()")
     public void before(){
         System.out.println("前置增强..........");
-    }
-
-    public void afterReturning(){
-        System.out.println("后置增强..........");
     }
 
     //Proceeding JoinPoint:  正在执行的连接点===切点
@@ -27,6 +31,7 @@ public class MyAspect {
         return proceed;
     }
 
+    @AfterThrowing("pointcut()")
     public void afterThrowing(){
         System.out.println("异常抛出增强..........");
     }
